482d5da7221031e51547aeb3529ceba757f1bee9
anshumanasahu <136312787+anshumanasahu@users.noreply.github.com>
2025-04-07 14:09:40 +0530

    Output fixed

--- compiler/codegen.py
+++ compiler/codegen.py
@@ -1,10 +1,14 @@
+from .parser import ASTNode, Var, ModAdd
+
 def generate(node):
-    if node.__class__.__name__ == "ModAdd":
+    if isinstance(node, ModAdd):
         a = generate(node.a)
         b = generate(node.b)
         m = generate(node.mod)
-        return f"(({a} % {m}) + ({b} % {m})) % {m}"
-    elif node.__class__.__name__ == "Var":
+        return f"result = (({a} % {m}) + ({b} % {m})) % {m}\nprint(result)"
+
+    elif isinstance(node, Var):
         return node.value
+
     else:
-        raise TypeError("Unknown node type")
+        raise TypeError(f"Unknown node type: {type(node)}")
